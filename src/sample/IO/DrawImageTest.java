b40ccc09b6cc459a5b5bb81924c6894ff68407bf
MrBaiJianKun <150443349@qq.com>
2019-04-11 06:36:35 +0800

    advance

---
+++ src/sample/IO/DrawImageTest.java
@@ -0,0 +1,26 @@
+package sample.IO;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawImageTest  extends JFrame {
+    public DrawImageTest() {
+        this.setSize(500, 380);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new CanvasTest());
+        this.setTitle("绘制图像");
+    }
+    public static void main(String[] args) {
+        new DrawImageTest().setVisible(true);
+    }
+    class CanvasTest extends Canvas {
+        public void paint(Graphics g) {
+            super.paint(g);
+            Graphics2D g2 = (Graphics2D) g;
+            Image img = new ImageIcon(this.getClass().getResource("/img/1.png")).getImage();
+            g2.drawImage(img, 0, 0, this);
+        }
+    }
+}
+
+
